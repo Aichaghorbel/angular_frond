43798684851f86d5c7d9da5d35cd3519759ed848
Aicha Ghorbel <aichagho26@gmail.com>
2026-05-12 00:23:46 +0100

    prepare deploy angular

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
-// src/environments/environment.prod.ts
+
 export const environment = {
   production: true,
-  apiUrl: 'https://ton-api-production.com/api'
-}
+  apiUrl: 'https://api-laravel-5f5a.onrender.com/api'
+};
