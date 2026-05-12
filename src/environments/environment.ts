4c31593b8b30c290e9e25bdebfcb680b39c35336
Aicha Ghorbel <aichagho26@gmail.com>
2026-05-12 10:22:22 +0100

    fix api url production

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
-// src/environments/environment.ts
+
 export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:8000/api'
+  production: true,
+  apiUrl: 'https://api-laravel-5f5a.onrender.com/api'
 };
